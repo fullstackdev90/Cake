a9703b520b9fe05bfad9c7104641e37ac344321a
fullstackdev90 <jarik2604@ukr.net>
2023-02-10 22:27:44 +0200

    first commit

---
+++ cake/script.js
@@ -0,0 +1,6 @@
+document.querySelector('#abutton').onclick = function (){
+    let dateStart =  document.querySelector('#start-date').value;
+    let timeStart =  document.querySelector('#start-time').value;
+    console.log(dateStart);
+    console.log(timeStart);
+}
